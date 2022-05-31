3a930a0e59f782d05f4ece943abe090b36cdf8db
jaeilnet <sgsgself@naver.com>
2022-05-31 18:01:23 +0900

    node 연습

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const api = require('./api');
+
+const app = new Koa();
+const router = new Router();
+
+// 라우트 설정
+router.use('/api', api.routes()); // api 라우트 적용
+
+// 라우터 적용 전에 bodyParser 적용
+
+// koa-bodyparser 미들웨어를 적용해야 합니다.
+// 이 미들웨어는 POST/PUT/PATCH 같 은 메서드의 Request Body에 JSON 형식으로 데이터를 넣어주면,
+//  이를 파싱하여 서버에서 사용할 수 있게 합니다.
+app.use(bodyParser());
+
+// app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log('http://localhost:4000/');
+});
