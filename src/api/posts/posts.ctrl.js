c670dde285e5703168bd60199d8f72ab05aebf7d
jaeilnet <sgsgself@naver.com>
2022-06-16 18:01:35 +0900

    mogon connect

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,7 +1,38 @@
 const Post = require('../../models/post');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const { ObjectId } = mongoose.Types;
+
+exports.checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+
+  return next();
+};
 
 exports.write = async (ctx) => {
   const { title, body, tags } = ctx.request.body;
+
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  console.log(schema);
   const post = new Post({
     title,
     body,
@@ -15,7 +46,51 @@ exports.write = async (ctx) => {
     ctx.throw(500, error);
   }
 };
-exports.list = (ctx) => {};
-exports.read = (ctx) => {};
-exports.remove = (ctx) => {};
-exports.update = (ctx) => {};
+
+exports.list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (error) {
+    ctx.throw(500, error);
+  }
+};
+
+exports.read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const posts = await Post.findById(id).exec();
+    ctx.body = posts;
+  } catch (error) {
+    ctx.throw(500, error);
+  }
+};
+
+exports.remove = async (ctx) => {
+  const { id } = ctx.params;
+
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (error) {
+    ctx.throw(500, error);
+  }
+};
+
+exports.update = async (ctx) => {
+  const { id } = ctx;
+
+  try {
+    const post = await Post.findOneAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (error) {
+    ctx.throw(500, error);
+  }
+};
